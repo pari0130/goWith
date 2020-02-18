3a67a4b3dae6858e554885beb2d7a069aaafed84
donghwi(ntb) <pari0130@gmail.com>
2020-02-18 15:44:47 +0900

    Initial commit

---
+++ src/main/java/com/jd/gowith/GowithApplication.java
@@ -0,0 +1,13 @@
+package com.jd.gowith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GowithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GowithApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jd/gowith/GowithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jd.gowith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GowithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
